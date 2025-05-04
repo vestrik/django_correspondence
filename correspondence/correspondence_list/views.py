99d0cd829b90682f2836722d9db33ff0c6b62450
vestrik <vestrik11@gmail.com>
2025-05-04 18:54:25 +0300

    Рефакторинг

--- correspondence/correspondence_list/views.py
+++ correspondence/correspondence_list/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
