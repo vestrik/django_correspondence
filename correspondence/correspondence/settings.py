cfe88a27ceb28bcc0304284aafadf628303e9739
vestrik <vestrik11@gmail.com>
2025-05-04 19:33:02 +0300

    Добавил автоудаление файлов

--- correspondence/correspondence/settings.py
+++ correspondence/correspondence/settings.py
@@ -36,9 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',    
+    'django_filters',
     'correspondence_list',
     'accounts',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
