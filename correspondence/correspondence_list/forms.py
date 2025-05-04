5a078e626ff18b3abe872bc84199037dcd90e007
vestrik <vestrik11@gmail.com>
2025-05-04 18:54:20 +0300

    Выпадающий список отделов

--- correspondence/correspondence_list/forms.py
+++ correspondence/correspondence_list/forms.py
@@ -1,10 +1,13 @@
 from django import forms
 from . import models
+from accounts.models import Department
 
 class DateInput(forms.DateInput):
     input_type = 'date'
 
 class CorrespondenceForm(forms.ModelForm):
+    department = forms.ModelChoiceField(queryset=Department.objects.all(), empty_label='')
+
     class Meta:
         model = models.Correspondence
         date = forms.DateField(widget=DateInput)
